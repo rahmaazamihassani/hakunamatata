b72fc392924665a8dac09cf8b733526ac48fa3a9
Rahma <azamihassanirahma@gmail.com>
2021-08-09 10:06:35 +0100

    Monday aug 9

--- force-app/main/default/lwc/lWC009_DossierInternSearchBar/lWC009_DossierInternSearchBar.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class LWC009_DossierInternSearchBar extends LightningElement {}
--- force-app/main/default/lwc/test_aux_expert/test_aux_expert.js
+++ force-app/main/default/lwc/test_aux_expert/test_aux_expert.js
@@ -5,103 +5,118 @@ import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 import VILLE_FIELD from '@salesforce/schema/Auxiliaire__c.Ville__c';
 import TYPE_EXPERT from '@salesforce/schema/Auxiliaire__c.TypeExpert__c'
 import AUXILIAIRE_OBJECT from  '@salesforce/schema/Auxiliaire__c';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import CreateRecordAffAux from '@salesforce/apex/DM004_AuxiliaireExpert.CreateRecordAffAux';
+import getExpertList from '@salesforce/apex/testExpert.getExpertList';
 
-import CreateRecordAffAux from '@salesforce/apex/DM003_AuxiliaireAvocat.CreateRecordAffAux';
-import getAuxiliaireList from '@salesforce/apex/customSearchSobjectLWC.getAuxiliaireList';
-
-export default class Testpopup extends LightningElement {
+export default class Test_aux_expert extends LightningElement {
     value ='';
     value2 = '';
-    valueType = '';
     @track auxRecord=[];
-    @track allRecord;
     @api recordId;
+    optionsv = [];
+    picklistOptions = [];
 
+    optionsT = [];
+    picklistOptionsT = [];
+    selectedValueT;
     searchValue = '';
 
     columns = [
         { label: 'Nom', fieldName: 'Name' },
-        { label: 'Type Expert', fieldName: 'TypeExpert__c' },
         { label: 'Ville', fieldName: 'Ville__c'  },
-        { label: 'Type Auxiliaire', fieldName: 'RecordType.DeveloperName' },
+        { label: "Type d'auxiliaire", fieldName: 'TypeAuxiliaire__c' },
+        { label: "Type d'expert", fieldName: 'TypeExpert__c' }
     ];
     type=true;
     typeExpert=false;
-    get options() {
-        return [
-            { label: 'Sélectionner un choix', value: 'Sélectionner un choix' },
-            { label: 'Avocat', value: 'Avocat' },
-            { label: 'Expert', value: 'Expert' },     
-        ];
-    }
+
+   
 
     @wire(getObjectInfo, { objectApiName: AUXILIAIRE_OBJECT })
     auxMetadata;
 
     @wire(getPicklistValues,{recordTypeId: '$auxMetadata.data.defaultRecordTypeId', fieldApiName: VILLE_FIELD})
-    villeAux;
+     wiredData( { error, data } ) {
 
-    @wire(getPicklistValues,{recordTypeId: '$auxMetadata.data.defaultRecordTypeId',fieldApiName: TYPE_EXPERT})
-    typeExpertAux;
-    // on select picklist value to show the selected value
-    handleChange(event) {
-        this.value = event.detail.value;
-    }
-    handleChangeType(event) {
-        this.valueType = event.detail.value;
-        if(this.valueType=='Expert'){
-            this.typeExpert = true;
+        if ( data ) {
+            var a = {label:"-Aucun-",value:"-Aucun-"};
+            this.optionsv = data.values.map( objPL => {
+ 
+                return {
+                    label: `${objPL.label}`,
+                    value: `${objPL.value}`
+                };
+            });
+            this.optionsv.unshift(a);
             
+        } else if ( error ) {
         }
-        else{
-            this.typeExpert = false;
-        }
-
 
     }
+    
+    @wire(getPicklistValues,{recordTypeId: '$auxMetadata.data.defaultRecordTypeId',fieldApiName: TYPE_EXPERT})
+    wiredDatai( { error, data } ) {
+
+        if ( data ) {
+            var a = {label:"-Aucun-",value:"-Aucun-"};
+            this.optionsT = data.values.map( objPL => {
+ 
+                return {
+                    label: `${objPL.label}`,
+                    value: `${objPL.value}`
+                };
+            });
+            this.optionsT.unshift(a);
+            
+        } else if ( error ) {
+        }
+
+    }
+    // on select picklist value to show the selected value
+
+    searchValueKey(event) {
+        this.value = event.target.value;
+        if(this.value=='-Aucun-'){
+            this.value = '';
+        }
+    }
+    
     handleChange2(event) {
 
-        this.value2 = event.detail.value;
+        this.value2 = event.target.value;
+        if(this.value2=='-Aucun-'){
+            this.value2 = '';
+        }
     }
    
-    
     // update searchValue var when input field value change
     searchKeyword(event) {
         this.searchValue = event.target.value;
     }
 
-    searchValueKey(event) {
-        this.value = event.target.value;
-        console.log(this.value);
-    }
 
-    searchValue2Key(event) {
-        this.value2 = event.target.value;
-        console.log(this.value2);
-
-    }
-
-
+    @track tableData;
     @track datahere=true;
     // call apex method on button click 
     handleSearchKeyword() {
-      
-        getAuxiliaireList({
-                valuekey: this.value,
-                recordType: this.valueType,
+        if(this.value!==''||this.value2!==''||this.searchValue!==''){
+        getExpertList({
                 value2key:this.value2,
+                valuekey: this.value,
                 searchKey: this.searchValue,
                 id:this.recordId
             })
 
                 .then(result => {
+                    console.log(result);
+                    // const v = result.filter(({ value: id1 }) => !this.temp.some(({ value: id2 }) => id2 === id1));
                     this.auxRecord = result;
-
+                    console.log(this.auxRecord);
                     this.datahere = false;
                     if(this.auxRecord.length ==0){
                         this.datahere=true;
                     }
-                    console.log(result);
                 })
                 .catch(error => {
                     this.datahere = true;
@@ -116,23 +131,32 @@ export default class Testpopup extends LightningElement {
                     // reset contacts var with null   
                     this.auxRecord = null;
                 });
-        
+            }
+           else{
+               this.auxRecord=undefined;
+           }
     }
-
    
     @track preselectedIds = [];
 getSelectedAux() {
     let selectedRows = this.template.querySelector('lightning-datatable').getSelectedRows();
     let rowsObject = JSON.parse(JSON.stringify(selectedRows));
-    console.log(rowsObject)
+    console.log(rowsObject);
     rowsObject.forEach(element => {
         let targetId = element.Id;
         let targetName = element.Name;
-        let targetType = element.RecordType.DeveloperName;
-        console.log(targetType);
-        CreateRecordAffAux({AuxId:targetId,DjId:this.recordId,AuxName:targetName,AuxType:targetType})
+        CreateRecordAffAux({AuxId:targetId,DjId:this.recordId,AuxName:targetName})
         .then(data=>{
             console.log(data);
+            this.auxRecord=undefined;
+            this.customHideModalPopup();
+            const event = new ShowToastEvent({
+                title: 'Expert affecté avec succès.',
+                variant: 'success'
+               
+            });
+            this.dispatchEvent(event);
+            window.location.reload();
         })
         .catch(error =>{
             console.log(error);
@@ -150,6 +174,6 @@ customShowModalPopup() {
 customHideModalPopup() {    
     
     this.customFormModal = false;
-}
 
+}
 }
